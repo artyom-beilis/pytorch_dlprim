984c800d838eb02c8761fab1b42a18bd4f89660f
Artyom Beilis <artyomtnk@yahoo.com>
2021-10-18 11:32:21 +0300

    Cache debuggin

--- CLTensor.h
+++ CLTensor.h
@@ -17,22 +17,32 @@ namespace ptdlprim {
         CLMemAllocation &operator=(CLMemAllocation &&) = default;
         ~CLMemAllocation() {}
 
-        CLMemAllocation(int id,cl::Context &ctx,std::int64_t length) :
+        CLMemAllocation(int id,cl::Context &ctx,std::int64_t length,std::int64_t os) :
             device_id(id),
             size(length),
+            orig_size(os),
             buffer(ctx,CL_MEM_READ_WRITE,length)
         {
         }
 
         int device_id;
         std::int64_t size;
+        std::int64_t orig_size;
         cl::Buffer buffer;
     };
 
     class CLCache {
     public:
-        typedef std::unordered_map<std::int64_t,std::list<std::unique_ptr<CLMemAllocation> > > allocation_type;
+        typedef std::map<std::int64_t,std::list<std::unique_ptr<CLMemAllocation> > > allocation_type;
         allocation_type allocation;
+
+        std::int64_t allocated_size = 0;
+        std::int64_t peak_requested_size = 0;
+        std::int64_t requested_size = 0;
+        std::int64_t cached_size = 0;
+
+        bool reuse_oversized_chunks = getenv("OPENCL_CACHE_OVERSIZED") && atoi(getenv("OPENCL_CACHE_OVERSIZED"));
+        bool debug_allocator = (getenv("OPENCL_DEBUG_CACHE") && atoi(getenv("OPENCL_DEBUG_CACHE")));
 
         void clear()
         {
@@ -55,24 +65,64 @@ namespace ptdlprim {
         }
 
 
-        std::unique_ptr<CLMemAllocation> allocate(int id,cl::Context &ctx,int64_t size)
+        std::unique_ptr<CLMemAllocation> allocate(int id,cl::Context &ctx,int64_t orig_size)
         {
-            size = round(size);
+            if(allocated_size == 0 && debug_allocator)
+                setlocale(LC_ALL,"");
+
+            std::int64_t size = round(orig_size);
             std::unique_ptr<CLMemAllocation> res;
+            
             auto p=allocation.find(size);
+            if(reuse_oversized_chunks) {
+                int times=0;
+                while(p!=allocation.end()) {
+                    if(!p->second.empty() || times==2) {
+                        break;
+                    }
+                    ++p;
+                    times++;
+                }
+            }
+
             if(p==allocation.end() || p->second.empty()) {
-                res.reset(new CLMemAllocation(id,ctx,size));
+                res.reset(new CLMemAllocation(id,ctx,size,orig_size));
+                allocated_size += res->size;
             }
             else {
                 res = std::move(p->second.back());
+                TORCH_CHECK(res->size >= orig_size,"Internal validation");
+                res->orig_size = orig_size;
+                cached_size -= res->size;
                 p->second.pop_back();
             }
+            requested_size += res->orig_size;
+            peak_requested_size = std::max(requested_size,peak_requested_size);
+            if(debug_allocator)
+                printf("malloc: allocated: %'16ld  requested %'16ld peak-req %'16ld cached %'16ld\n",allocated_size,requested_size,peak_requested_size,cached_size);
             return res;
         }
         void release(std::unique_ptr<CLMemAllocation> &&mem)
         {
             int64_t size = mem->size;
+            cached_size += mem->size;
+            requested_size -= mem->orig_size;
+            if(debug_allocator)
+                printf("free  : allocated: %'16ld  requested %'16ld peak-req %'16ld cached %'16ld\n",allocated_size,requested_size,peak_requested_size,cached_size);
             allocation[size].push_back(std::move(mem));
+        }
+
+        void prepare(dlprim::Context &ctx)
+        {
+            int64_t mem_size = ctx.device().getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>();
+            int64_t rounded_mem_size = round(mem_size);
+            for(int64_t size=1;size<=rounded_mem_size;size*=2) {
+                allocation[size]; // create empty list
+            }
+            if(debug_allocator) {
+                setlocale(LC_ALL,"");
+                printf("GPU max memory allocation size %'15ld creating tables up to %'ld\n",mem_size,rounded_mem_size);
+            }
         }
     };
 
@@ -201,6 +251,7 @@ namespace ptdlprim {
                 return res;
             res.ctx=dlprim::Context(res.name);
             res.queue = res.ctx.make_execution_context();
+            res.cache.prepare(res.ctx);
             res.ready = true;
             std::cout << "Accessing device #" << i << ":" << res.ctx.name() << std::endl;
             return res;
